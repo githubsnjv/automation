85c0aeb2bed960540e717156eebd98b6dd921743
githubsnjv <snjvacoz@gmail.com>
2023-03-15 22:25:24 +0530

    form frontend and details pushing

--- src/app/crud.service.ts
+++ src/app/crud.service.ts
@@ -16,4 +16,17 @@ export class CrudService {
     return this.afs.collection("/final").valueChanges();
   }
 
+  //gets the current id no to create the job no
+  current_id(){
+    return this.afs.collection("/final").doc("current_id").valueChanges();
+  }
+
+  current_id_add(cur_id:number){
+    this.afs.collection("/final").doc("current_id").set({id:cur_id});
+  }
+
+  add_form_data(o:any){
+    this.afs.collection("/final").doc(this.afs.createId()).set(o);
+  }
+
 }
